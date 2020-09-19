034213410405aa70fbffde7e1076a30d4350e2b7
David Castner <davidjcastner@gmail.com>
2020-09-19 11:08:29 -0400

    Update pe_016.py

--- src/problems/pe_016.py
+++ src/problems/pe_016.py
@@ -7,8 +7,8 @@
 from typing import List
 
 
-def multiple_digits(digit_list: List[int], multiplier: int, base: int = 10) -> List[int]:
-    '''multiples a list of digits by a small number, each digit in the list shoudl represent the value digit_list[index] * base ** index'''
+def multiply(digit_list: List[int], multiplier: int, base: int = 10) -> List[int]:
+    '''multiplies a list of digits by a small number, each digit in the list shoudl represent the value digit_list[index] * base ** index'''
     product, magnitude, remainder = [], 0, 0
     while remainder > 0 or magnitude < len(digit_list):
         if magnitude < len(digit_list):
@@ -24,7 +24,7 @@ def solve() -> int:
     base, exponent = 2, 1000
     product = [1]
     for x in range(exponent):
-        product = multiple_digits(product, base)
+        product = multiply(product, base)
     return sum(product)
 
 
