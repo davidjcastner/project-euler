a0adf6dafe725b58d0a29e6f44cd852ece142d7d
David Castner <davidjcastner@gmail.com>
2020-09-23 10:19:20 -0400

    rewrite s_001 to pass linting and testing

--- project_euler/solutions/s_001.py
+++ project_euler/solutions/s_001.py
@@ -1,27 +1,25 @@
 # Multiples of 3 and 5
 # Problem 1
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-
-def int_range_sum(start: int, end: int, divisor: int) -> int:
-    '''returns the sum of all integers in the range [start, end] divisible by divisor'''
-    calc_start = start // divisor
-    calc_end = end // divisor
-    return ((calc_start + calc_end) * (calc_end - calc_start + 1) // 2) * divisor
+from valkyrie_util.calculate import range_sum_int
 
 
-def solve() -> int:
+def solve(divisor_a: int = 3, divisor_b: int = 5, range_start: int = 0, range_end: int = (1000 - 1)) -> str:
     '''Problem 1 - Multiples of 3 and 5'''
     # can be done with math:
     # sum of multiples of 3 in the range [1, 1000)
     # + sum of multiples of 5 in the range [1, 1000)
     # - sum of multiples of 15 in the range [1, 1000)
-    start, end = 0, 999
-    return int_range_sum(start, end, 3) + int_range_sum(start, end, 5) - int_range_sum(start, end, 15)
+    a = range_sum_int(range_start, range_end, divisor_a)
+    b = range_sum_int(range_start, range_end, divisor_b)
+    c = range_sum_int(range_start, range_end, divisor_a * divisor_b)
+    return str(a + b - c)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(solve())
