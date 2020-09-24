4a4bd8354ba4e2feda11c96ec495c36347aa7a5f
David Castner <davidjcastner@gmail.com>
2020-09-23 20:43:40 -0400

    Update s_010.py

--- project_euler/solutions/s_010.py
+++ project_euler/solutions/s_010.py
@@ -4,27 +4,18 @@
 
 # Find the sum of all the primes below two million.
 
-from typing import Generator
+from valkyrie_util.primes import prime_iterator
 
 
-def primes(max_prime: int) -> Generator[int, None, None]:
-    '''generates the prime numbers using a sieve'''
-    max_prime = max(max_prime, 2)
-    is_prime = [True] * (max_prime + 1)
-    is_prime[0] = False
-    is_prime[1] = False
-    for number in range(max_prime + 1):
-        if is_prime[number]:
-            for multiple in range(number ** 2, max_prime + 1, number):
-                is_prime[multiple] = False
-            yield number
-
-
-def solve() -> int:
+def solve(limit: int = 2 * 10 ** 6) -> str:
     '''Problem 10 - Summation of primes'''
-    limit = 2 * 10 ** 6
-    return sum(primes(limit - 1))
+    prime_sum = 0
+    for prime in prime_iterator():
+        if prime >= limit:
+            break
+        prime_sum += prime
+    return str(prime_sum)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(solve())
