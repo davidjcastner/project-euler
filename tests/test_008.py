bf1317f8ee97ae6bd4f133b001d7bc13d6a459ab
David Castner <davidjcastner@gmail.com>
2020-09-23 20:26:30 -0400

    rewrite s_008 to pass tests and linting

--- tests/test_008.py
+++ tests/test_008.py
@@ -3,7 +3,7 @@ from project_euler.solutions.s_008 import solve
 
 def test_s_008() -> None:
     # basic example
-    answer = solve(series_length=4, data_file='./data/d_008.txt')
+    answer = solve(factor_count=4, data_file='d_008.txt')
     assert type(answer) == str
     assert answer == '5832'
 
