2186dfb5964de73be6b887fbaa6096246c24f178
David Castner <davidjcastner@gmail.com>
2020-09-25 22:55:20 -0400

    solve s_029

---
+++ tests/test_029.py
@@ -0,0 +1,13 @@
+from project_euler.solutions.s_029 import solve
+
+
+def test_simplified_version() -> None:
+    answer = solve(base=range(2, 6), exponent=range(2, 6))
+    assert type(answer) == str
+    assert answer == '15'
+
+
+def test_answer() -> None:
+    answer = solve()
+    assert type(answer) == str
+    assert answer == '9183'
