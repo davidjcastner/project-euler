15616677debc79d7b8f92f1361965f2bc8e04fd9
David Castner <davidjcastner@gmail.com>
2021-05-03 10:18:59 -0400

    start rework of matrix lib

--- src/lib/struct/Matrix.py
+++ src/lib/struct/Matrix.py
@@ -1,74 +1,78 @@
 from __future__ import annotations
 
-from typing import Iterable
-from typing import Union
+from typing import Generator
+from typing import Generic
+from typing import TypeVar
 
 
-class Matrix:
-    '''2d list of integers'''
+T = TypeVar('T')
 
-    def __init__(self, rows: int, cols: int) -> None:
-        assert rows > 0 and cols > 0
+
+class Matrix(Generic[T]):
+    '''two dimensional matrix of data, cannot be empty'''
+
+    def __init__(self, rows: int, cols: int, default_value: T = 0) -> None:
+        assert isinstance(rows, int) and rows > 0
+        assert isinstance(cols, int) and cols > 0
+        self.__default = default_value
         self.rows = rows
         self.cols = cols
-        self.data = [[0 for col in range(cols)] for row in range(rows)]
-
-    def __str__(self) -> str:
-        s = [[str(val) for val in row] for row in self.data]
-        s = [(', ').join(row) for row in s]
-        s = [f'[ {row} ]' for row in s]
-        return ('\n').join(s)
+        self.size = rows * cols
+        self.data = [default_value for _ in range(self.size)]
 
     @staticmethod
-    def from_data(data: list[list[int]]) -> None:
+    def from_data(data: list[list[T]]) -> None:
+        assert isinstance(data, list) and len(data) > 0
+        assert isinstance(data[0], list) and len(data[0]) > 0
         rows = len(data)
         cols = len(data[0])
-        assert rows > 1
         assert all((len(row) == cols for row in data))
         matrix = Matrix(rows, cols)
         matrix.data = data
         return matrix
 
+    def __repr__(self):
+        row = f'rows: {self.rows}'
+        col = f'cols: {self.cols}'
+        data = f'data: {self.data}'
+        return f'Matrix({row}, {col}, {data})'
+
     def blank_copy(self) -> Matrix:
-        return Matrix(self.rows, self.cols)
+        return Matrix(self.rows, self.cols, self.__default)
 
-    def get(
-            self,
-            row: Union[int | None] = None,
-            col: Union[int | None] = None,
-            index: Union[int | None] = None) -> int:
-        assert (
-            row is not None and col is not None and index is None
-        ) or (
-            row is None and col is None and index is not None
-        )
-        use_index = index is not None
-        if use_index:
-            assert False, 'Not implemented'
-        else:
-            return self.data[row][col]
+    def get_index(self, row: int, col: int) -> int:
+        '''returns the index for the row and col in the matrix'''
+        assert isinstance(row, int) and row >= 0 and row < self.rows
+        assert isinstance(col, int) and col >= 0 and col < self.cols
+        return row * self.cols + col
 
-    def set(
-            self,
-            row: Union[int | None] = None,
-            col: Union[int | None] = None,
-            index: Union[int | None] = None,
-            value: int = 0) -> int:
-        assert (
-            row is not None and col is not None and index is None
-        ) or (
-            row is None and col is None and index is not None
-        )
-        use_index = index is not None
-        if use_index:
-            assert False, 'Not implemented'
-        else:
-            self.data[row][col] = value
+    def get_row_col(self, index: int) -> tuple[int, int]:
+        '''returns the (row, col) for the index in the matrix'''
+        assert isinstance(index, int) and index >= 0 and index < self.size
+        return (index // self.cols, index % self.cols)
 
-    def each_row_col(self, reverse: bool = False) -> Iterable[tuple[int, int], None, None]:
+    def __iter__(self):
+        for val in self.data:
+            yield val
+
+    def __getitem__(self, key: int):
+        return self.data[key]
+
+    def iter_rows_cols(
+        self,
+        reverse: bool = False
+    ) -> Generator[tuple[int, int], None, None]:
+        assert isinstance(reverse, bool)
         for row in range(self.rows):
             for col in range(self.cols):
                 if reverse:
                     yield self.rows - 1 - row, self.cols - 1 - col
                 else:
                     yield row, col
+
+    def sub_sequences(
+        self,
+        # direction: D,
+        length: int
+    ) -> Generator[list[T], None, None]:
+        pass
